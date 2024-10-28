ea3409eccaabd7f2e91a4ca80008bfe2e2b45d15
Ramy <176412840+Rbenkov-24@users.noreply.github.com>
2024-10-28 04:13:49 -0400

    Add footer component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from './components/Footer';
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h4>Contact Us</h4>
+          <p>
+            Email: <span>info@skillstream.com</span>
+          </p>
+          <div>
+            <h4>Subscribe to our newsletter</h4>
+            <form>
+              <input type="email" placeholder="Your email" required />
+              <button type="submit">Subscribe</button>
+            </form>
+          </div>
+        </div>
+
+        <div>
+          <h4>Follow Us</h4>
+          <a href="#">Facebook</a>
+          <a href="#">Twitter</a>
+          <a href="#">Instagram</a>
+          <a href="#">LinkedIn</a>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/terms">Terms of Service</a>
+          <a href="/faq">FAQ</a>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>© 2024 SkillStream, INC.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
